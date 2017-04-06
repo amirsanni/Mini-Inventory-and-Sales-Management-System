be50ee7b00288290719b69ce3b2dff16f9e7c6fc
Amir <amirsanni@gmail.com>
2017-04-06 10:59:20 +0100

    View and Print Transaction Within Specific Dates

---
+++ application/views/transactions/transReport.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') OR exit('');
+
+/**
+ * @fileName transReport
+ * @author Ameer <amirsanni@gmail.com>
+ * @date 06-Apr-2017
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Transaction Report</title>
+		
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="<?=base_url()?>public/images/icon.ico">
+        <!-- favicon ends --->
+        
+        <!--- LOAD FILES -->
+        <?php if((stristr($_SERVER['HTTP_HOST'], "localhost") !== FALSE) || (stristr($_SERVER['HTTP_HOST'], "192.168.") !== FALSE)|| (stristr($_SERVER['HTTP_HOST'], "127.0.0.") !== FALSE)): ?>
+        <link rel="stylesheet" href="<?=base_url()?>public/bootstrap/css/bootstrap.min.css">
+
+        <?php else: ?>
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <?php endif; ?>
+        
+        <!-- custom CSS -->
+        <link rel="stylesheet" href="<?= base_url() ?>public/css/main.css">
+    </head>
+
+    <body>
+        <div class="container margin-top-5">
+            <div class="row hidden-print">
+                <div class="col-xs-12 text-right">
+                    <button class="btn btn-primary btn-sm" onclick="window.print()">Print Report</button>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <h4>Transactions Between <?=date('jS M, Y', strtotime($from))?> and <?=date('jS M, Y', strtotime($to))?></h4>
+                </div>
+            </div>
+            
+            <div class="row margin-top-5">
+                <div class="col-xs-12 table-responsive">
+                    <div class="panel panel-primary">
+                        <!-- Default panel contents -->
+                        <div class="panel-heading text-center">
+                            Transactions Between <?=date('jS M, Y', strtotime($from))?> and <?=date('jS M, Y', strtotime($to))?>
+                        </div>
+                        <?php if($allTransactions): ?>
+                        <div class="table table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>SN</th>
+                                        <th>Receipt No</th>
+                                        <th>Total Items</th>
+                                        <th>Total Amount</th>
+                                        <th>Amount Tendered</th>
+                                        <th>Change Due</th>
+                                        <th>Mode of Payment</th>
+                                        <th>Staff</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $sn = 1; ?>
+                                    <?php foreach($allTransactions as $get): ?>
+                                    <tr>
+                                        <th><?= $sn ?>.</th>
+                                        <td><?= $get->ref ?></td>
+                                        <td><?= $get->quantity ?></td>
+                                        <td>&#8358;<?= number_format($get->totalMoneySpent, 2) ?></td>
+                                        <td>&#8358;<?= number_format($get->amountTendered, 2) ?></td>
+                                        <td>&#8358;<?= number_format($get->changeDue, 2) ?></td>
+                                        <td><?=  str_replace("_", " ", $get->modeOfPayment)?></td>
+                                        <td><?=$get->staffName?></td>
+                                        <td><?= date('jS M, Y h:i:sa', strtotime($get->transDate)) ?></td>
+                                    </tr>
+                                    <?php $sn++; ?>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- table div end--->
+                        <?php else: ?>
+                            <ul><li>No Transaction Found Within Specified Dates</li></ul>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row hidden-print">
+                <div class="col-xs-12">
+                    <button class="btn btn-primary btn-sm" onclick="window.print()">Print Report</button>
+                </div>
+            </div>
+        </div>
+        <!--- panel end-->
+    </body>
+</html>
--- application/views/transactions/transactions.php
+++ application/views/transactions/transactions.php
@@ -10,6 +10,8 @@ if(isset($items) && !empty($items)){
 }
 ?>
 
+<style href="<?=base_url('public/ext/datetimepicker/bootstrap-datepicker.min.css')?>" rel="stylesheet"></style>
+
 <script>
     var currentItems = <?=json_encode($current_items)?>;
 </script>
@@ -19,9 +21,16 @@ if(isset($items) && !empty($items)){
         <div class="col-sm-12">
             <!--- Row to create new transaction-->
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-3">
                     <span class="pointer text-primary">
                         <button class='btn btn-primary btn-sm' id='showTransForm'><i class="fa fa-plus"></i> New Transaction </button>
+                    </span>
+                </div>
+                <div class="col-sm-3">
+                    <span class="pointer text-primary">
+                        <button class='btn btn-primary btn-sm' data-toggle='modal' data-target='#reportModal'>
+                            <i class="fa fa-newspaper-o"></i> Generate Report 
+                        </button>
                     </span>
                 </div>
             </div>
@@ -221,5 +230,51 @@ if(isset($items) && !empty($items)){
 </div>
 
 
+<div class="modal fade" id='reportModal' data-backdrop='static' role='dialog'>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="close" data-dismiss='modal'>&times;</div>
+                <h4 class="text-center">Generate Report</h4>
+            </div>
+            
+            <div class="modal-body">
+                <div class="row" id="datePair">
+                    <div class="col-sm-6 form-group-sm">
+                        <label class="control-label">From Date</label>                                    
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <span><i class="fa fa-calendar"></i></span>
+                            </div>
+                            <input type="text" id='transFrom' class="form-control date start" placeholder="YYYY-MM-DD">
+                        </div>
+                        <span class="help-block errMsg" id='transFromErr'></span>
+                    </div>
+
+                    <div class="col-sm-6 form-group-sm">
+                        <label class="control-label">To Date</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <span><i class="fa fa-calendar"></i></span>
+                            </div>
+                            <input type="text" id='transTo' class="form-control date end" placeholder="YYYY-MM-DD">
+                        </div>
+                        <span class="help-block errMsg" id='transToErr'></span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="modal-footer">
+                <button class="btn btn-success" id='clickToGen'>Generate</button>
+                <button class="btn btn-danger" data-dismiss='modal'>Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!---End of copy of div to clone when adding more items to sales transaction---->
 <script src="<?=base_url()?>public/js/transactions.js"></script>
+<script src="<?=base_url('public/ext/datetimepicker/bootstrap-datepicker.min.js')?>"></script>
+<script src="<?=base_url('public/ext/datetimepicker/jquery.timepicker.min.js')?>"></script>
+<script src="<?=base_url()?>public/ext/datetimepicker/datepair.min.js"></script>
+<script src="<?=base_url()?>public/ext/datetimepicker/jquery.datepair.min.js"></script>
