e92190ad4580e97896e825c879a77ba2534f037e
ShantuRashid <42793675+ShantuRashid@users.noreply.github.com>
2023-04-13 06:07:39 -0300

    chang2

--- system/libraries/Upload.php
+++ system/libraries/Upload.php
@@ -654,7 +654,7 @@ class CI_Upload {
 	{
 		if ($this->encrypt_name === TRUE)
 		{
-			$filename = md5(uniqid(mt_rand())).$this->file_ext;
+			$filename = sha256(uniqid(mt_rand())).$this->file_ext;
 		}
 
 		if ($this->overwrite === TRUE OR ! file_exists($path.$filename))
